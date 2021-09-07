fc18bc582d0dc1959f386c316c53748e6ae41fc4
Accacio <raccacio@poli.ufrj.br>
2021-09-07 17:59:32 -0300

    tex plot

--- script/mpcRun.m
+++ script/mpcRun.m
@@ -1,4 +1,4 @@
-clear
+close all; clear
 clc
 simK=10;
 Te=0.5;
@@ -6,6 +6,7 @@ n=5;
 
 % options = optimset('Display', 'off')
 opts = optimoptions('quadprog', 'MaxIter', 200, 'Display','off');
+% opts = optimoptions('quadprog', 'Display','off');
 
 X0(:,1) = [18.3 3.0]';
 
@@ -18,7 +19,7 @@ Ro=[0.5];
 Cres=[5];
 Cs=[8];
 
-Wt(:,:,1) = [20*ones(1,floor(simK/2)) 21*ones(1,simK-floor(simK/2))];
+Wt(:,:,1) = [20*ones(1,5) 21*ones(1,5)];
 
 A=@(Cres,Cs,Rf,Ri,Ro) [-1/(Cres*Rf)-1/(Cres*Ri) 1/(Cres*Ri); 1/(Cs*Ri) -1/(Cs*Ro)-1/(Cs*Ri)];
 B=@(Cres,Cs,Rf,Ro) [10/Cres 1/(Cres*Rf); 0 1/(Cs*Ro)];
@@ -84,11 +85,15 @@ end
 
 %%
 for K=1:simK
-    stairs(1:simK,Wt,'r')
+%   subplot(2,1,1);
+    figure
+    stairs(1:simK,Wt,'LineWidth',1.5,'Color',[0.5 0.5 0.5])
     hold on
-    plot(1:K,xt(1,1:K),'b*')
-    stairs(K:K+n,[xt(1,K) xtPred(:,K)'],'k--')
+    plot(1:K,xt(1,1:K),'^','Color',[0.47 0.12 0.22],'MarkerFaceColor',[0.47 0.12 0.22])
+    plot(K+1:K+n,[xtPred(:,K)'],'^','LineWidth',1,'Color',([1 1 1]+[0.47 0.12 0.22])/2)
+    patch([K+1 K+1 K+n K+n],[18 22 22 18],[.47 .12 .22],'FaceAlpha',.1,'EdgeColor','none')
     hold off
+    ylabel('Temperature (^oC)')
     xlim([1 simK])
     ylim([18 22])
     set(gcf, 'PaperPosition', [0 0 8 7])
@@ -96,18 +101,37 @@ for K=1:simK
     set(gca,'FontSize',18)
     set(gca,'FontName','cmr18')
     set(gcf, 'PaperSize', [8 7])
+    set(gcf,'defaulttextinterpreter','latex')
     hx=get(gca,'xlabel');
     hy=get(gca,'ylabel');
     hz=get(gca,'zlabel');
     ht=get(gca,'title');
     hl=legend();
-    lgd=legend({'Setpoint' 'State' 'Prediction'},'Location','southeast');
+    lgd=legend({'Setpoint' 'State' 'Prediction'},'Location','northwest');
     lgd.FontSize = 16;
     lgd.FontWeight = 'bold';
     set(hx,'FontSize',18)
     set(hy,'FontSize',18)
     set(hz,'FontSize',18)
     set(hl,'FontSize',18)
-
+    title('Temperature in Room')
+    matlab2tikz(['../img/mpcState' num2str(K) '.tex'], 'height', '\fheight', 'width', '\fwidth','showInfo', false );
+    
+%     subplot(2,1,2)
+    close
+    figure
+    stairs(K:K+n,[uHist(:,K)' uHist(end,K)],'--','LineWidth',1,'Color',([1 1 1]+[0.47 0.12 0.22])/2)
+    hold on
+    stairs(1:K,uHist(1,1:K),'k-','Color',[0.47 0.12 0.22])
+    patch([K K K+n K+n],[0 2 2 0],[.47 .12 .22],'FaceAlpha',.1,'EdgeColor','none')
+    hold off
+    lgd=legend({'Prediction' 'Applied'},'Location','southeast');
+    ylabel('Power (kW)')
+    title('Control')
+    xlim([1 simK])
+    ylim([0 2])
+    
+    matlab2tikz(['../img/mpcControl' num2str(K) '.tex'], 'height', '\fheight', 'width', '\fwidth','showInfo', false);
     saveas(gcf,['../img/mpc' num2str(K) '.pdf'])
+    close
 end
